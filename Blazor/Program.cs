29b96dc3da9d4ea4d35ddf770da58a0c649dfd8c
Scrumlord123 <94675757+Scrumlord123@users.noreply.github.com>
2022-10-16 01:17:50 +0200

    Git start

--- Blazor/Program.cs
+++ Blazor/Program.cs
@@ -1,13 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor;
-using shared.Model;
+using Blazor.Data;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ApiService>();
 
 builder.Services.AddOidcAuthentication(options =>
 {
--- Blazor/Services/ApiService.cs
+++ Blazor/Services/ApiService.cs
@@ -22,7 +22,7 @@ public class ApiService
 
     public async Task<Post[]> GetPosts()
     {
-        string url = $"{baseAPI}posts/";
+        string url = $"{baseAPI}/api/posts";
         return await http.GetFromJsonAsync<Post[]>(url);
     }
 
@@ -34,7 +34,7 @@ public class ApiService
 
  public async Task<Comment> CreateComment(string content, int postId, string Author)
     {
-        string url = $"{baseAPI}posts/{postId}/comments";
+        string url = $"{baseAPI}/api/posts/{postId}/comments";
      
         // Post JSON to API, save the HttpResponseMessage
         HttpResponseMessage msg = await http.PostAsJsonAsync(url, new { content, Author });
@@ -50,7 +50,29 @@ public class ApiService
         // Return the new comment 
         return newComment;
     }
+
+
+
+      public async Task<Post> UpvotePost(int id)
+    {
+        string url = $"{baseAPI}/api/posts/{id}/upvote/";
+
+        // Post JSON to API, save the HttpResponseMessage
+        HttpResponseMessage msg = await http.PutAsJsonAsync(url, "");
+
+        // Get the JSON string from the response
+        string json = msg.Content.ReadAsStringAsync().Result;
+
+        // Deserialize the JSON string to a Post object
+        Post? updatedPost = JsonSerializer.Deserialize<Post>(json, new JsonSerializerOptions {
+            PropertyNameCaseInsensitive = true // Ignore case when matching JSON properties to C# properties
+        });
+
+        // Return the updated post (vote increased)
+        return updatedPost;
+    }
 }
+
 
 
 //mangler comment, upvote og downvote
